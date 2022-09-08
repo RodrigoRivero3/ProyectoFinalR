ebb35651fd96c6c21269b739b6e75933d0853cbb
Rodrigo Rivero <rodrigo.rivero@hotmail.com.ar>
2022-09-07 21:24:07 -0300

    proyecto terminado

--- AppGame/urls.py
+++ AppGame/urls.py
@@ -9,5 +9,13 @@ urlpatterns = [
     path('juegos/',juegos, name='AppGameJuegos'),
     path('jugador_formulario/', jugador_formulario, name='AppGameJugadorFormulario'),
     path('busqueda_apodo/', busqueda_apodo, name='AppGameBusquedaJugadorApodo'),
-    path('busqueda_apodo_post/', busqueda_apodo_post, name='AppGameBusquedaJugadorApodoPost')
+    path('busqueda_apodo_post/', busqueda_apodo_post),
+    path('consola_formulario/', consola_formulario , name='AppGameConsolaFormulario'),
+    path('busqueda_modelo/', busqueda_modelo, name='AppGameBusquedaConsolaModelo'),
+    path('busqueda_modelo_post/', busqueda_modelo_post),
+    path('juegos_formulario/', juegos_formulario, name='AppGameJuegosFormulario'),
+    path('busqueda_categoria/', busqueda_categoria, name='AppGameBusquedaJuegosCategoria'),
+    path('busqueda_categoria_post/', busqueda_categoria_post)
+
+    
 ]
